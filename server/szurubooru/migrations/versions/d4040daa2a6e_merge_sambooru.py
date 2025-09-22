761d176dce9d6037b6f2aeb99f7e60986b929d95
udf <tabhooked@gmail.com>
2025-09-22 17:32:44 +0200

    fix alembic merge conflict

---
+++ server/szurubooru/migrations/versions/d4040daa2a6e_merge_sambooru.py
@@ -0,0 +1,21 @@
+'''
+Merge revision to combine the head on rr-/szurubooru with udf/szurubooru
+
+Revision ID: d4040daa2a6e
+Created at: 2025-09-22 15:25:22.640415
+'''
+
+import sqlalchemy as sa
+from alembic import op
+
+
+revision = 'd4040daa2a6e'
+down_revision = ('5b5c940b4e78', '6be3bcde9204')
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
